659dcb87913d7868517238d75780a9cfb8fce15d
s1o3urn <d4vid4pplepie@gmail.com>
2019-01-24 22:51:39 -0500

    detect black line

--- ca/mcgill/ecse211/lab2/OdometryCorrection.java
+++ ca/mcgill/ecse211/lab2/OdometryCorrection.java
@@ -1,9 +1,18 @@
 package ca.mcgill.ecse211.lab2;
 
+import lejos.hardware.*;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.utility.Delay;
+
 public class OdometryCorrection implements Runnable {
   private static final long CORRECTION_PERIOD = 10;
   private Odometer odometer;
-
+  EV3ColorSensor colourSensor;
+  private int xCounter = 0;
+  private int yCounter = 0;
+  private double error = 0.2;
   /**
    * This is the default class constructor. An existing instance of the odometer is used. This is to
    * ensure thread safety.
@@ -13,6 +22,8 @@ public class OdometryCorrection implements Runnable {
   public OdometryCorrection() throws OdometerExceptions {
 
     this.odometer = Odometer.getOdometer();
+    //Create a colour sensor and attach to a port
+    colourSensor = new EV3ColorSensor(SensorPort.S4);
 
   }
 
@@ -27,10 +38,20 @@ public class OdometryCorrection implements Runnable {
 
     while (true) {
       correctionStart = System.currentTimeMillis();
-
+      
+      //Display current X and Y as if starting from initial position
+      LCD.drawInt(xCounter, 0, 4);
+      LCD.drawInt(yCounter, 0, 5);
+      
       // TODO Trigger correction (When do I have information to correct?)
+      //Check if a black line exists
+      if(colourSensor.getColorIDMode().equals("1"));
+      Delay.msDelay(100);
+      Sound.beep();
+      
       // TODO Calculate new (accurate) robot position
-
+      
+      
       // TODO Update odometer with new calculated (and more accurate) vales
 
       odometer.setXYT(0.3, 19.23, 5.0);
