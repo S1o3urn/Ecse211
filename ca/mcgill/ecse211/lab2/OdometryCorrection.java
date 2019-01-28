591625aac58223222c4cb4b3f0f1bf9504f81304
s1o3urn <d4vid4pplepie@gmail.com>
2019-01-27 19:01:07 -0500

    correction retry

--- ca/mcgill/ecse211/lab2/OdometryCorrection.java
+++ ca/mcgill/ecse211/lab2/OdometryCorrection.java
@@ -9,7 +9,7 @@ import lejos.robotics.SampleProvider;
 import lejos.hardware.Sound;
 
 public class OdometryCorrection implements Runnable {
-	private static final double TILE_MEASURE = 30.48;
+	private static final double TILE_MEASURE = 30; //30.48
 	private static final long CORRECTION_PERIOD = 10;
 	private Odometer odometer;
 
@@ -22,9 +22,14 @@ public class OdometryCorrection implements Runnable {
 
 	private float lastValue = 0;
 
-	private int xCounter = 0; // Keeps track of how many tiles driven on the x-axis
-	private int yCounter = 0; // Keeps track of how many tiles driven on the y-axis
+	private int xCounterEastbound = 0; // Keeps track of how many tiles driven on the x-axis
+	private int yCounterNorthbound = 0; 
+	private int xCounterWestbound = 0;
+	private int yCounterSouthbound  = 0;
 	private double theta; // Absolute angle of the robot in relation with a fixed plane
+	
+	private double startYDistance;
+	private double startXDistance;
 
 	private double[] position;
 
@@ -76,26 +81,55 @@ public class OdometryCorrection implements Runnable {
 				// Split into 4 directions for navigation
 				// East side
 				if ((theta > 315 && theta <= 360) || (theta >= 0 && theta <= 45)) {
-					odometer.setX(yCounter * TILE_MEASURE);
-					xCounter++;
+					if(xCounterEastbound == 0) {
+						position = odometer.getXYT();
+						startXDistance = position[0];
+						xCounterEastbound++;
+					}
+					
+					else {
+						xCounterEastbound++;
+						odometer.setX((xCounterEastbound * TILE_MEASURE) - (TILE_MEASURE - startXDistance));
+					}
 				}
 
 				// North side
 				else if (theta > 45 && theta <= 135) {
-					odometer.setY(xCounter * TILE_MEASURE);
-					yCounter++;
+					if(yCounterNorthbound == 0) {
+						position = odometer.getXYT();
+						startYDistance = position[1];
+						yCounterNorthbound++;
+					}
+					else {
+						yCounterNorthbound++;
+						odometer.setY((yCounterNorthbound * TILE_MEASURE) - (TILE_MEASURE - startYDistance));
+					}
 				}
 
 				// West side
 				else if (theta > 135 && theta <= 225) {
-					xCounter--;
-					odometer.setX(yCounter * TILE_MEASURE);
+					if(xCounterWestbound == 0) {
+						position = odometer.getXYT();
+						startXDistance = (3 * TILE_MEASURE) - position[1];
+						xCounterWestbound++;
+					}
+					else {
+						xCounterWestbound++;
+						odometer.setX((xCounterWestbound * TILE_MEASURE) - startXDistance);
+					}
 				}
 
 				// South zone
 				else if (theta > 225 && theta <= 315) {
-					yCounter--;
-					odometer.setY(xCounter * TILE_MEASURE);
+					if(yCounterSouthbound == 0) {
+						position = odometer.getXYT();
+						startYDistance = (3 * TILE_MEASURE) - position[1];
+						yCounterSouthbound++;
+					}
+					else {
+						yCounterSouthbound++;
+						odometer.setY((yCounterSouthbound * TILE_MEASURE) - startYDistance);
+					}
 				}
 
 				try {
