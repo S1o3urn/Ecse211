492c8ffe984c7aaadecee7d392c9ca46dcac4682
s1o3urn <d4vid4pplepie@gmail.com>
2019-02-03 15:52:23 -0500

    fixed odometer class

--- ca/mcgill/ecse211/lab3/EV3Navigation.java
+++ ca/mcgill/ecse211/lab3/EV3Navigation.java
@@ -10,48 +10,51 @@ import lejos.hardware.sensor.SensorModes;
 import lejos.robotics.SampleProvider;
 
 public class EV3Navigation {
-	
-	//Initialize class variables
+
+	// Initialize class variables
+	// Motors
 	private static final EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
 	private static final EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
 	private static final EV3LargeRegulatedMotor sensorMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("B"));
 	private static final Port ultrasonicSensorPort = LocalEV3.get().getPort("S4");
-	
-	//Wheel constants
-	public static final double WHEEL_RADIUS = 2.13;
-	public static final double WHEEL_BASE = 15.13;
-	
+
+	// Wheel constants
+	public static final double WHEEL_RADIUS = 2.3;
+	public static final double WHEEL_BASE = 13.25;
+
 	public static void main(String[] args) {
-		
-		// Setting up the sensor
-		@SuppressWarnings("resource")							    						// Since ultrasonic sensor is always on
-		SensorModes ultrasonicSensor = new EV3UltrasonicSensor(ultrasonicSensorPort);		// ultrasonicSensor being the instance
-		SampleProvider ultrasonicDistance = ultrasonicSensor.getMode("Distance");			// ultrasonicDistance provides samples from this instance
-		float[] ultrasonicData = new float[ultrasonicDistance.sampleSize()];				// ultrasonicData is the buffer in which data are returned
-		
-		
-		//display
+
+		// Sensor setup
+		@SuppressWarnings("resource") // Since ultrasonic sensor is always on
+		SensorModes ultrasonicSensor = new EV3UltrasonicSensor(ultrasonicSensorPort); // ultrasonicSensor is the
+																						// instance
+		SampleProvider ultrasonicDistance = ultrasonicSensor.getMode("Distance"); // ultrasonicDistance provides samples
+																					// from this instance
+		float[] ultrasonicData = new float[ultrasonicDistance.sampleSize()]; // ultrasonicData is the buffer in which
+																				// data are returned
+
+		// Display
 		int buttonPressed;
 		
 		final TextLCD text = LocalEV3.get().getTextLCD();
 		Odometer odometer = new Odometer(leftMotor, rightMotor);
 		Display odometryDisplay = new Display(odometer, text);
 		Navigation navigation = new Navigation(odometer, leftMotor, rightMotor);
-		ObstacleAvoidanceNavigation advancedNavigation = new ObstacleAvoidanceNavigation(odometer,leftMotor, rightMotor, sensorMotor, ultrasonicDistance, ultrasonicData);
-		
-		
+		ObstacleAvoidanceNavigation advancedNavigation = new ObstacleAvoidanceNavigation(odometer, leftMotor,
+				rightMotor, sensorMotor, ultrasonicDistance, ultrasonicData);
+
 		do {
 			text.clear();
-			
+
 			text.drawString("< Left | Right >", 0, 0);
 			text.drawString("       |        ", 0, 1);
 			text.drawString(" Simple|   adv  ", 0, 2);
 			text.drawString(" Nav   |   Nav  ", 0, 3);
-			
+
 			buttonPressed = Button.waitForAnyPress();
 		} while (buttonPressed != Button.ID_LEFT && buttonPressed != Button.ID_RIGHT);
-		
-		if (buttonPressed == Button.ID_LEFT){
+
+		if (buttonPressed == Button.ID_LEFT) {
 			odometer.start();
 			odometryDisplay.start();
 			navigation.start();
@@ -60,9 +63,9 @@ public class EV3Navigation {
 			odometryDisplay.start();
 			advancedNavigation.start();
 		}
-		
-		while (Button.waitForAnyPress() != Button.ID_ESCAPE);
+
+		while (Button.waitForAnyPress() != Button.ID_ESCAPE)
+			;
 		System.exit(0);
 	}
 }
-
