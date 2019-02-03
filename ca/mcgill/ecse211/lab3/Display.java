c6134087cafaab50f1e5f390cc8b5df1ff900d17
s1o3urn <d4vid4pplepie@gmail.com>
2019-02-03 16:17:40 -0500

    fixed display.java

--- ca/mcgill/ecse211/lab3/Display.java
+++ ca/mcgill/ecse211/lab3/Display.java
@@ -1,96 +1,64 @@
 package ca.mcgill.ecse211.lab3;
 
+import java.text.DecimalFormat;
+
+/**
+ * This class implements the logic behind the display 
+ * while the robot is running in either simple navigation 
+ * or obstacle avoidance navigation
+ * 
+ * adapted from lab2 Display.java
+ * and previous years Display.java
+ */
 import lejos.hardware.lcd.TextLCD;
 
 public class Display extends Thread {
-	
-	private static final long DISPLAY_PERIOD = 250;
-	private Odometer odometer;
-	private TextLCD text;
 
-	// constructor
-	public Display(Odometer odometer, TextLCD text) {
+	private Odometer odometer;
+	private TextLCD LCD;
+	private static final long DISPLAY_PERIOD = 25;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param odometer
+	 * @param text
+	 */
+	public Display(Odometer odometer, TextLCD lcd) {
 		this.odometer = odometer;
-		this.text = text;
+		this.LCD = lcd;
 	}
 
 	// run method (required for Thread)
 	public void run() {
-		long displayStart, displayEnd;
+		long displayStart;
+		long displayEnd;
 		double[] position = new double[3];
 
-		// clear the display once
-		text.clear();
+		// Reset display
+		LCD.clear();
 
 		while (true) {
 			displayStart = System.currentTimeMillis();
 
-			// clear the lines for displaying odometry information
-			text.drawString("X:              ", 0, 0);
-			text.drawString("Y:              ", 0, 1);
-			text.drawString("T:              ", 0, 2);
-
-			// get the odometry information
+			// Retrieve odometer position information
 			odometer.getPosition(position, new boolean[] { true, true, true });
 
 			// display odometry information
-			for (int i = 0; i < 3; i++) {
-				text.drawString(formattedDoubleToString(position[i], 2), 3, i);
-			}
+			DecimalFormat numberFormat = new DecimalFormat("######0.00");
+			LCD.drawString("X: " + numberFormat.format(position[0]), 0, 0);
+			LCD.drawString("Y: " + numberFormat.format(position[1]), 0, 1);
+			LCD.drawString("T: " + numberFormat.format(position[2]), 0, 2);
 
-			// throttle the OdometryDisplay
+			// Check to make sure update only happens every period
 			displayEnd = System.currentTimeMillis();
 			if (displayEnd - displayStart < DISPLAY_PERIOD) {
 				try {
 					Thread.sleep(DISPLAY_PERIOD - (displayEnd - displayStart));
 				} catch (InterruptedException e) {
-					// there is nothing to be done here because it is not
-					// expected that OdometryDisplay will be interrupted
-					// by another thread
+					e.printStackTrace();
 				}
 			}
 		}
 	}
-	
-	private static String formattedDoubleToString(double x, int places) {
-		String result = "";
-		String stack = "";
-		long text;
-		
-		// Put in a minus sign as needed
-		if (x < 0.0)
-			result += "-";
-		
-		// put in a leading 0
-		if (-1.0 < x && x < 1.0)
-			result += "0";
-		else {
-			text = (long)x;
-			if (text < 0)
-				text = -text;
-			
-			while (text > 0) {
-				stack = Long.toString(text % 10) + stack;
-				text /= 10;
-			}
-			
-			result += stack;
-		}
-		
-		// put the decimal, if needed
-		if (places > 0) {
-			result += ".";
-		
-			// put the appropriate number of decimals
-			for (int i = 0; i < places; i++) {
-				x = Math.abs(x);
-				x = x - Math.floor(x);
-				x *= 10.0;
-				result += Long.toString((long)x);
-			}
-		}
-		
-		return result;
-	}
-
 }
